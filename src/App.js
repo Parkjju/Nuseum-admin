b7aee6fff3c957b78538cb2f96b859ef0b3ad64e
Parkjju <rudwns3927@gmail.com>
2022-10-11 14:19:10 +0900

    feat: axios defaults 설정 / 헬멧 컴포넌트 등록 / 글로벌스타일 정의

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
+import axios from 'axios';
+import { Helmet } from 'react-helmet';
+import { GlobalStyle } from './components/atom/Global';
+import { Container, Contents } from './components/pages/Home/styled';
+import Today from './components/pages/Today/Today';
+import Router from './router';
+
+axios.defaults.baseURL = 'https://www.nuseum.site';
+axios.defaults.withCredentials = true;
+
 function App() {
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <Helmet>
+                <link
+                    rel='stylesheet'
+                    href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0'
+                />
+                <link
+                    href='https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@200;300;400;500;600;700;900&display=swap'
+                    rel='stylesheet'
+                />
+            </Helmet>
+            <GlobalStyle />
+            <Router />
+        </>
     );
 }
 
