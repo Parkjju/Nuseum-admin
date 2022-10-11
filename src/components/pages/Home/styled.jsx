a5d2a0661be74aaf8fe6ca2c5e333aee45e8198c
Parkjju <rudwns3927@gmail.com>
2022-10-11 17:47:52 +0900

    feat: 유저리스트 클릭 컴포넌트 Link 상속

--- src/components/pages/Home/styled.jsx
+++ src/components/pages/Home/styled.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Logo = styled.img`
@@ -16,4 +17,43 @@ export const Container = styled.div`
     margin: 0 auto;
     padding-top: 40px;
     min-height: 600px;
+`;
+
+export const UserTab = styled(Link)`
+    width: 80%;
+    height: 54px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 25px;
+    border-bottom: 1px solid black;
+    max-width: 480px;
+    color: black;
+    &:hover {
+        opacity: 0.8;
+        transition: 0.1s linear;
+        cursor: pointer;
+    }
+    &:visited {
+        color: black;
+    }
+    text-decoration: none;
+`;
+
+export const AnalysisBtn = styled(Link)`
+    height: 32px;
+    width: 68px;
+    background-color: #e3e3e3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: black;
+    text-decoration: none;
+    border-radius: 100px;
+    cursor: pointer;
+    border: none;
+    &:active {
+        opacity: 0.8;
+        transition: 0.1s linear;
+    }
 `;
