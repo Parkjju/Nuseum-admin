872265016fe8fcde9162e5a9f53271cad374cd3a
Parkjju <rudwns3927@gmail.com>
2022-10-21 07:26:35 +0900

    feat: Recommend  styled 컴포넌트 생성 / 타이틀 및 input관련 스타일 변경

--- src/components/pages/Today/components/Recommend.jsx
+++ src/components/pages/Today/components/Recommend.jsx
@@ -1,6 +1,99 @@
-import RecommmendTab from './RecommendTab';
+import React, { useState } from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { groupActions } from '../../../../store/group-slice';
+import { TabBox, TabContents, TabInput, TabTitle } from './Recommend.styled';
 
-const Recommend = () => {
-    return <RecommmendTab></RecommmendTab>;
+const Recommend = ({ name, index }) => {
+    const [main, setMain] = useState('');
+    const [subText, setSubText] = useState('');
+    const group = useSelector((state) => state.group.group);
+    const dispatch = useDispatch();
+
+    const onChangeMain = (e) => {
+        setMain(e.target.value);
+        const modifiedIndex = group.data.findIndex(
+            (item) => item.type === name
+        );
+        dispatch(
+            groupActions.updateMain({
+                newElement: e.target.value,
+                index: modifiedIndex,
+            })
+        );
+    };
+    const onChangeSub = (e) => {
+        setSubText(e.target.value);
+    };
+    const onKeyDown = (e) => {
+        if (e.keyCode === 13) {
+            if (e.target.value === '') return;
+            const modifiedIndex = group.data.findIndex(
+                (item) => item.type === name
+            );
+            dispatch(
+                groupActions.updateContent({
+                    index: modifiedIndex,
+                    newElement: e.target.value,
+                })
+            );
+
+            setSubText('');
+        }
+    };
+
+    return (
+        <Draggable draggableId={name} index={index}>
+            {(magic) => (
+                <TabBox
+                    ref={magic.innerRef}
+                    {...magic.dragHandleProps}
+                    {...magic.draggableProps}
+                >
+                    <TabTitle>
+                        {name === '주의' ? '피해야할 음식' : name}
+                    </TabTitle>
+                    <TabContents>
+                        <TabInput
+                            value={main}
+                            onChange={onChangeMain}
+                            placeholder='대표 음식 1가지'
+                        />
+                        <TabInput
+                            onKeyDown={onKeyDown}
+                            onChange={onChangeSub}
+                            value={subText}
+                            placeholder='추천 음식 4가지'
+                        />
+
+                        <div
+                            style={{
+                                display: 'flex',
+                                marginTop: 5,
+                                width: '100%',
+                                flexWrap: 'wrap',
+                            }}
+                        >
+                            {group.data
+                                .filter((item) => item.type === name)[0]
+                                .list.map((item, index) => (
+                                    <span
+                                        style={{
+                                            marginRight: 5,
+                                            fontSize: 12,
+                                            marginBottom: 5,
+                                        }}
+                                        key={index}
+                                    >
+                                        {item}
+                                    </span>
+                                ))}
+                        </div>
+                    </TabContents>
+                </TabBox>
+            )}
+        </Draggable>
+    );
 };
-export default Recommend;
+export default React.memo(Recommend);
--- src/components/pages/Today/components/Recommend.styled.js
+++ src/components/pages/Today/components/Recommend.styled.js
@@ -1,24 +1,24 @@
 import styled from 'styled-components';
 
 export const TabBox = styled.div`
-    width: 80%;
+    width: 90%;
     box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
     background-color: white;
     height: 84px;
     padding: 20px;
     display: flex;
     align-items: center;
-    max-width: 400px;
+    max-width: 460px;
     margin: 0 auto;
+    margin-bottom: 30px;
+    cursor: pointer;
 `;
 
-export const TabTitle = styled.p`
-    width: 46px;
-    height: 36px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-right: 30px;
+export const TabTitle = styled.span`
+    margin-right: 20px;
+    width: 80px;
+    text-align: center;
+    line-height: 1.5;
 `;
 
 export const TabContents = styled.div`
@@ -34,7 +34,8 @@ export const TabInput = styled.input`
     height: 20px;
     font-size: 13px;
     width: 100%;
-    border: 1px solid #bfc5c6;
+    border: none;
+    border-bottom: 1px solid #bfc5c6;
     &:focus {
         outline: none;
     }
