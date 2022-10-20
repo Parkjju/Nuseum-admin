8028177a9f0b06c6f161d1738e406d95350ed355
Parkjju <rudwns3927@gmail.com>
2022-10-21 07:21:33 +0900

    feat: memoization을 위해 전역상태 슬라이싱으로 그룹변수 분리

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './auth-slice';
 import dateSlice from './date-slice';
+import groupSlice from './group-slice';
 
 const store = configureStore({
     reducer: {
         auth: authSlice,
         date: dateSlice,
+        group: groupSlice,
     },
 });
 
