43ba55065485ca8bd235ff9ce231c25dab40830a
Parkjju <rudwns3927@gmail.com>
2022-10-25 18:09:39 +0900

    feat: groupSlice 리듀서 추가 ==== 1. initialState에 comment속성 추가 2. sortNeed플래그 기준으로 fetch 직후의 배열인지 여부 판단 / order값 기준으로 정렬할지 id값 기준으로 정렬할지 판단 3. initialState에 isFetched플래그 추가 - PATCH or POST요청 분기를 위해 사용 4. fetched / notFetched - isFetched 수정을 위한 리듀서 5. isFetched는 맞춤식품 포스팅에 대한 아이디값을 갖는다. 6. removeAll리듀서 추가 - 달력 onchange시 전체 전역상태값 초기상태로 초기화 7. updateComment리듀서 추가 - 코멘트 수정 관련 리듀서 추가

--- src/store/group-slice/index.js
+++ src/store/group-slice/index.js
@@ -88,20 +88,35 @@ const initialState = {
                 id: 11,
             },
         ],
+        comment: '',
     },
+    isFetched: null,
 };
 const groupSlice = createSlice({
     name: 'group',
     initialState,
     reducers: {
         updateGroup(state, action) {
-            state.group = { data: [...action.payload] };
-        },
-        updateOrder(state, action) {
-            let idx = 0;
-            for (let i of action.payload.newOrder) {
-                state.group.data[i].order = idx;
-                idx += 1;
+            if (action.payload?.sortNeed) {
+                let copy = [...action.payload.sorted];
+                console.log('dispatch sorted', action.payload.sorted);
+                let arr = [...copy.sort((item1, item2) => item1.id - item2.id)];
+                console.log('arr', arr);
+                state.group = {
+                    data: [...arr],
+                    comment: action.payload.comment,
+                };
+            } else {
+                state.group = {
+                    data: [...action.payload.data],
+                    comment: action.payload.comment,
+                };
+
+                let id = 0;
+                for (let index in state.group.data) {
+                    state.group.data[index].id = id;
+                    id += 1;
+                }
             }
         },
         updateContent(state, action) {
@@ -112,6 +127,111 @@ const groupSlice = createSlice({
         updateMain(state, action) {
             state.group.data[action.payload.index].main =
                 action.payload.newElement;
+        },
+        removeContent(state, action) {
+            state.group.data[action.payload.id].list.splice(
+                action.payload.index,
+                1
+            );
+        },
+        fetched(state, action) {
+            state.isFetched = action.payload;
+        },
+        notFetched(state) {
+            state.isFetched = null;
+        },
+        removeAll(state) {
+            state.group.comment = '';
+            state.group.data = [
+                {
+                    type: '과일',
+                    main: '',
+                    list: [],
+                    order: 0,
+                    id: 0,
+                },
+                {
+                    type: '채소',
+                    main: '',
+                    list: [],
+                    order: 1,
+                    id: 1,
+                },
+                {
+                    type: '콩/두부',
+                    main: '',
+                    list: [],
+                    order: 2,
+                    id: 2,
+                },
+                {
+                    type: '통곡물',
+                    main: '',
+                    list: [],
+                    order: 3,
+                    id: 3,
+                },
+                {
+                    type: '버섯',
+                    main: '',
+                    list: [],
+                    order: 4,
+                    id: 4,
+                },
+                {
+                    type: '해조류',
+                    main: '',
+                    list: [],
+                    order: 5,
+                    id: 5,
+                },
+                {
+                    type: '견과',
+                    main: '',
+                    list: [],
+                    order: 6,
+                    id: 6,
+                },
+                {
+                    type: '고기/생선/달걀',
+                    main: '',
+                    list: [],
+                    order: 7,
+                    id: 7,
+                },
+                {
+                    type: '유제품',
+                    main: '',
+                    list: [],
+                    order: 8,
+                    id: 8,
+                },
+                {
+                    type: '가공식품',
+                    main: '',
+                    list: [],
+                    order: 9,
+                    id: 9,
+                },
+                {
+                    type: '영양제',
+                    main: '',
+                    list: [],
+                    order: 10,
+                    id: 10,
+                },
+                {
+                    type: '주의',
+                    main: '',
+                    list: [],
+                    order: 11,
+                    id: 11,
+                },
+            ];
+            state.isFetched = false;
+        },
+        updateComment(state, action) {
+            state.group.comment = action.payload;
         },
     },
 });
