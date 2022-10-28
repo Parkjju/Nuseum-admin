d47449f80bc707e447a60cac00afe405cea7817f
Parkjju <rudwns3927@gmail.com>
2022-10-28 14:16:48 +0900

    feat: Preview 미리보기 컴포넌트 라우터 등록

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Today from './components/pages/Today/Today';
 import Login from './components/pages/Login';
 import Home from './components/pages/Home';
 import Analysis from './components/pages/Analysis';
+import Preview from './components/pages/Preview';
 
 function Router() {
     return (
@@ -14,6 +15,7 @@ function Router() {
                 <Route element={<Login />} path='/login' />
                 <Route element={<Home />} path='/' />
                 <Route element={<Today />} path='/:userId' />
+                <Route element={<Preview />} path='/:userId/preview' />
                 <Route element={<Analysis />} path='/:userId/analysis' />
             </Routes>
         </BrowserRouter>
